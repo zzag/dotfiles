329f63415af8c64a4beab17a8982bffb2cc42e4e
Vlad Zagorodniy <vladzzag@gmail.com>
2019-02-05 02:51:12 +0200

    Add .profile.fish

---
+++ .profile.fish
@@ -0,0 +1,13 @@
+# Load keychain.
+if type -q keychain and status is-interactive
+    keychain --quiet --agents ssh id_github id_kde
+end
+
+if test -f ~/.keychain/(hostname)-fish
+    source ~/.keychain/(hostname)-fish
+end
+
+# Use ccache.
+if type -q ccache
+    set -gx fish_user_paths /usr/lib/ccache/bin $fish_user_paths
+end
