b969de5cdf5cc1826d2f7c6fe635c91740b53579
Vlad Zahorodnii <vladzzag@gmail.com>
2020-09-20 16:48:32 +0300

    fish: Add some explanatory comments

--- .profile.fish
+++ .profile.fish
@@ -3,5 +3,8 @@ if type -q ccache
     set -gx fish_user_paths /usr/lib/ccache/bin $fish_user_paths
 end
 
+# Use portals to ensure that GTK applications use native file dialogs.
 set -gx GTK_USE_PORTAL 1
+
+# For pixel perfect scrolling in Firefox.
 set -gx MOZ_USE_XINPUT2 1
