1d53605c3b49d4bc2386006440386e0d533bf48a
Vlad Zahorodnii <vladzzag@gmail.com>
2020-10-01 21:55:46 +0300

    fish: Update profile file

--- .profile.fish
+++ .profile.fish
@@ -3,6 +3,10 @@ if type -q ccache
     set -gx fish_user_paths /usr/lib/ccache/bin $fish_user_paths
 end
 
+if type -q rustup
+    set -gx fish_user_paths $HOME/.cargo/bin $fish_user_paths
+end
+
 # Use portals to ensure that GTK applications use native file dialogs.
 set -gx GTK_USE_PORTAL 1
 
