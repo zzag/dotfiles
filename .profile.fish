14ca6ad7940bcdd0b073d00f4b7d8420a4f7d3bd
Vlad Zahorodnii <vlad.zahorodnii@kde.org>
2023-03-11 18:23:45 +0200

    arch-setup: Reshuffle MOZ_ENABLE_WAYLAND

--- .profile.fish
+++ .profile.fish
@@ -3,8 +3,5 @@ if type -q ccache
     set -gx fish_user_paths /usr/lib/ccache/bin $fish_user_paths
 end
 
-# Enable Wayland support in Firefox and Thunderbird.
-set -gx MOZ_ENABLE_WAYLAND 1
-
 # Enable stem darkening (so fonts look more consistent in Qt and GTK apps)
 set -gx FREETYPE_PROPERTIES "truetype:interpreter-version=40 cff:no-stem-darkening=0 cff:darkening-parameters=500,500,1000,350,1667,350,2333,0"
