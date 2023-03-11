afddf1d0d650b1dea3d54519eb83d41403f7d5b6
Vlad Zahorodnii <vlad.zahorodnii@kde.org>
2023-03-11 18:24:22 +0200

    arch-setup: Reshuffle FREETYPE_PROPERTIES stuff

--- .profile.fish
+++ .profile.fish
@@ -2,6 +2,3 @@
 if type -q ccache
     set -gx fish_user_paths /usr/lib/ccache/bin $fish_user_paths
 end
-
-# Enable stem darkening (so fonts look more consistent in Qt and GTK apps)
-set -gx FREETYPE_PROPERTIES "truetype:interpreter-version=40 cff:no-stem-darkening=0 cff:darkening-parameters=500,500,1000,350,1667,350,2333,0"
